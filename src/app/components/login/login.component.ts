43d165b4f29344cdb66fdc2b9aeb3e05a63f69af
Çağatay Koç <kocagatayy@gmail.com>
2023-06-27 00:28:44 +0300

    .

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl,FormBuilder,Validator, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
@@ -16,6 +17,8 @@ export class LoginComponent implements OnInit{
     private formBuilder : FormBuilder,
     private authService : AuthService,
     private toastrService : ToastrService,
+    private localStorage : LocalStorageService,
+    private router: Router
     
   ){}
 
@@ -32,7 +35,7 @@ export class LoginComponent implements OnInit{
 
   login(){
     if (this.loginForm.valid) {
-      //console.log(this.loginForm.value);
+      console.log(this.loginForm.value);
       let loginModel = Object.assign({},this.loginForm.value)
       this.authService.login(loginModel).subscribe(data=>{
         this.toastrService.success(data.message)
@@ -42,5 +45,22 @@ export class LoginComponent implements OnInit{
       })
     }
   }
+
+  // signIn(){
+  //   if (this.loginForm.valid) {
+  //    // console.log(this.signInForm.value);
+  //     let loginModel=Object.assign({},this.loginForm.value)
+  //     this.authService.login(loginModel).subscribe(response=>{
+  //       localStorage.setItem('token', response.data.token)
+  //       this.router.navigate(["/"]);
+  //       this.toastrService.info("Signed In")
+        
+  //     },responseError=>{
+  //       //console.log(responseError);
+  //       this.toastrService.error(responseError.error)
+  //     })
+  //   }
+  // }
+
 
 }
